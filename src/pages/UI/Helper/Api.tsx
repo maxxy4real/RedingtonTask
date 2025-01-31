15a829c78d1cb0c0ae67f93091adb5d96202253c
Reivynn Chiang <reivynn.chiang@redington.co.uk>
2025-01-31 15:36:00 +0000

    poc

--- src/pages/UI/Helper/Api.tsx
+++ src/pages/UI/Helper/Api.tsx
@@ -1,6 +1,23 @@
+import { ImageType } from "./ImageConsts";
+
 export const GetAllImages = async () => {
   const res = await fetch(
     `'http://localhost:' + ${process.env.PORT}/api/images`
+  );
+  const data = await res.json();
+  return data;
+};
+
+export const AddImage = async (image: ImageType) => {
+  const res = await fetch(
+    `'http://localhost:' + ${process.env.PORT}/api/images`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(image),
+    }
   );
   const data = await res.json();
   return data;
